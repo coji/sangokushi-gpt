13520a508838b7cd5fdabdbfec898948c4f39f15
coji <coji@techtalk.jp>
2023-03-26 19:00:35 +0900

    embed

--- scripts/convert/structure.ts
+++ scripts/convert/structure.ts
@@ -1,5 +1,8 @@
+import { encoding_for_model } from '@dqbd/tiktoken'
 import fs from 'fs'
 import path from 'path'
+import { hashChunk } from 'scripts/utils/hashChunk'
+import type { Section } from 'types/model'
 import { parseText } from '../utils/parseText'
 
 const inputDir = 'data/sangokushi_cleaned'
@@ -19,21 +22,27 @@ const files = [
   '12hengaiyoroku.txt',
 ]
 
+// token カウントのためのエンコーダー
+const encoder = encoding_for_model('gpt-3.5-turbo')
+
 // 出力ディレクトリが存在しない場合、作成
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir)
 }
 
-const chapters = []
+const chapters: Omit<Section, 'vector' | 'usage'>[] = []
 // 各ファイルに処理を適用
 for (const file of files) {
   const inputFilePath = path.join(inputDir, file)
 
   const data = fs.readFileSync(inputFilePath, 'utf-8')
   const fileChapters = parseText(data)
+
   for (const chapter of fileChapters) {
     chapters.push({
+      id: hashChunk(chapter.content),
       file: path.parse(file).name,
+      tokens: encoder.encode(chapter.content).length,
       ...chapter,
     })
   }
@@ -41,3 +50,5 @@ for (const file of files) {
 
 const outputFilePath = path.join(outputDir, 'sangokushi.json')
 fs.writeFileSync(outputFilePath, JSON.stringify(chapters, null, 2))
+
+encoder.free()
---
+++ types/model.ts
@@ -0,0 +1,13 @@
+export interface Section {
+  id: string
+  file: string
+  tokens: number
+  volumeTitle: string
+  chapterNumber: number
+  chapterTitle: string
+  sectionNumber: string
+  startLineNumber: number
+  content: string
+  vector: number[]
+  usage: number
+}
