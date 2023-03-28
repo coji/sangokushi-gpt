02a5161ae9a876b2a33d0f94253c1462cb81029c
coji <coji@techtalk.jp>
2023-03-28 22:12:35 +0900

    fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,18 +22,16 @@ import { json, type ActionArgs } from '@vercel/remix'
 import invariant from 'tiny-invariant'
 import { fetchEmbedding } from '~/services/openai-embedding.server'
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { property: 'og:title', content: '三国志 GPT' },
-    { property: 'og:url', content: 'sangokushi-gpt.vercel.app' },
-    { property: 'og:description', content: '三国志の世界をChatGPTで探索。' },
-    { property: 'og:image', content: '/resource/ogp' },
-    { property: 'og:image:width', content: '1200' },
-    { property: 'og:image:height', content: '630' },
-    { property: 'og:twitter:card', content: 'summary_large_image' },
-    { property: 'og:twitter:site', content: '@techtalkjp' },
-  ]
-}
+export const meta: V2_MetaFunction = () => [
+  { property: 'og:title', content: '三国志 GPT' },
+  { property: 'og:url', content: 'sangokushi-gpt.vercel.app' },
+  { property: 'og:description', content: '三国志の世界をChatGPTで探索。' },
+  { property: 'og:image', content: '/resource/ogp' },
+  { property: 'og:image:width', content: '1200' },
+  { property: 'og:image:height', content: '630' },
+  { property: 'og:twitter:card', content: 'summary_large_image' },
+  { property: 'og:twitter:site', content: '@techtalkjp' },
+]
 
 export const action = async ({ request }: ActionArgs) => {
   const formData = await request.formData()
