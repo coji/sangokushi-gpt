c85e9d25d546a689c3faf661ccedc95a8ed0b576
coji <coji@techtalk.jp>
2023-03-29 05:53:07 +0900

    root

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,24 @@ export const meta: V2_MetaFunction = () => [
   { title: '三国志 GPT' },
   { charSet: 'utf-8' },
   { name: 'viewport', content: 'width=device-width,initial-scale=1' },
-  { name: 'description', content: '三国志についてなんでも質問できる GPT です' },
+  {
+    name: 'description',
+    content: '三国志についてなんでも答えてくれる GPT です',
+  },
+  { property: 'og:title', content: '三国志 GPT' },
+  { property: 'og:url', content: 'https://sangokushi-gpt.vercel.app/' },
+  {
+    property: 'og:description',
+    content: '三国志についてなんでも答えてくれる GPT です',
+  },
+  {
+    property: 'og:image',
+    content: 'https://sangokushi-gpt.vercel.app/resource/ogp',
+  },
+  { property: 'og:image:width', content: '1200' },
+  { property: 'og:image:height', content: '630' },
+  { property: 'og:twitter:card', content: 'summary_large_image' },
+  { property: 'og:twitter:site', content: '@techtalkjp' },
 ]
 
 interface DocumentProps {
