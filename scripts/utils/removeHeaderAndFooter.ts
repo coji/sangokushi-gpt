4574c38f92ac7dce8ff0895dcddd05ada8e43f03
coji <coji@techtalk.jp>
2023-04-02 18:37:53 +0900

    fix: test

--- scripts/utils/removeHeaderAndFooter.ts
+++ scripts/utils/removeHeaderAndFooter.ts
@@ -5,7 +5,7 @@ export function removeHeaderAndFooter(text: string): string {
   text = text.replace(headerPattern, '')
 
   // 末尾の注釈を除去
-  const footerPattern = /底本：(.|\r\n)*?青空文庫作成ファイル：([\s\S]*?)$/
+  const footerPattern = /底本：(.|\n)*?青空文庫作成ファイル：([\s\S]*?)$/
   text = text.replace(footerPattern, '')
 
   return text
