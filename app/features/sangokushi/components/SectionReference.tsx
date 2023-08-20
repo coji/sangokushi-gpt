cd108894ffcd563c91ea780e802abcb263f32d31
coji <coji@techtalk.jp>
2023-08-21 00:13:58 +0900

    fix: reference layout

--- app/features/sangokushi/components/SectionReference.tsx
+++ app/features/sangokushi/components/SectionReference.tsx
@@ -1,8 +1,6 @@
-import { Link } from '@remix-run/react'
-import { ExternalLinkIcon } from 'lucide-react'
 import React from 'react'
 import nl2br from 'react-nl2br'
-import { Button, Card, CardContent, CardHeader, HStack, Popover, PopoverContent, PopoverTrigger } from '~/components/ui'
+import { Button, HStack, Popover, PopoverContent, PopoverTrigger } from '~/components/ui'
 import type { Section } from '~/types/model'
 
 interface SectionReferenceProps {
@@ -18,32 +16,17 @@ export const SectionReference = ({ section, children }: SectionReferenceProps) =
 
       <Popover>
         <PopoverTrigger asChild>
-          <Button size="sm" variant="outline">
+          <Button size="xs" variant="link">
             原文
           </Button>
         </PopoverTrigger>
         <PopoverContent>
-          <Card>
-            <CardHeader>
-              <HStack>
-                <p>{section.chapterNumber}</p>
-                <p>{section.chapterTitle}</p>
-                <p>{section.sectionNumber}</p>
-                <Button asChild size="sm" variant="outline">
-                  <ExternalLinkIcon className="mr-2" />
-                  <Link
-                    to="https://github.com/coji/sangokushi-gpt/blob/main/app/features/sangokushi/actions/generate-action.server.ts#L14"
-                    target="_blank"
-                  >
-                    Prompt
-                  </Link>
-                </Button>
-              </HStack>
-            </CardHeader>
-            <CardContent>
-              <div className="h-[20rem] overflow-auto">{nl2br(section.content)}</div>
-            </CardContent>
-          </Card>
+          <HStack className="font-bold">
+            <p>{section.chapterNumber}</p>
+            <p>{section.chapterTitle}</p>
+            <p>{section.sectionNumber}</p>
+          </HStack>
+          <div className="h-[20rem] overflow-auto">{nl2br(section.content)}</div>
         </PopoverContent>
       </Popover>
     </React.Fragment>
