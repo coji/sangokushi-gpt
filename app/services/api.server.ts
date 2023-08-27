fb198a062d3ef5775704fcce785341fdb2cc943c
coji <coji@techtalk.jp>
2023-08-27 14:15:07 +0900

    fix: change embedding api to POST method

--- app/services/api.server.ts
+++ app/services/api.server.ts
@@ -8,6 +8,7 @@ export const search = async (query: string, top_k = 10) => {
 export const fetchEmbedding = async (text: string) => {
   const ret = await fetch(`${process.env.API_BASE_URL}/embedding`, {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ sentence: text }),
   })
   return (await ret.json()) as { embedding: number[] }
