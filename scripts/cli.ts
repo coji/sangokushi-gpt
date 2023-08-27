03f0652181845d0da54c895eb7f57aa5d94eb360
coji <coji@techtalk.jp>
2023-08-27 14:15:23 +0900

    remove unused

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -3,12 +3,7 @@ import { match } from 'ts-pattern'
 import { $ } from 'zx'
 
 const argv = cli({
-  commands: [
-    command({ name: 'cleanup' }),
-    command({ name: 'structure' }),
-    command({ name: 'embed' }),
-    command({ name: 'qdrant-upsert' }),
-  ],
+  commands: [command({ name: 'cleanup' }), command({ name: 'structure' })],
 })
 
 const main = async () => {
@@ -18,12 +13,6 @@ const main = async () => {
     })
     .with('structure', async () => {
       await $`tsx scripts/convert/02_structure.ts`
-    })
-    .with('embed', async () => {
-      await $`tsx scripts/convert/03_embed.ts`
-    })
-    .with('qdrant-upsert', async () => {
-      await $`tsx scripts/convert/04_qdrant-upsert-index.ts`
     })
     .otherwise(() => {
       argv.showHelp()
