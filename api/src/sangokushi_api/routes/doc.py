82afb20eb5aecab88003c7f9dd7257d9e632392e
coji <coji@techtalk.jp>
2023-08-29 11:01:41 +0900

    refactor: typed

--- api/src/sangokushi_api/routes/doc.py
+++ api/src/sangokushi_api/routes/doc.py
@@ -1,31 +1,48 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from ..services.database import db
 
 router = APIRouter()
 
 
-@router.get("/doc")
-def root():
+class Doc(BaseModel):
+    id: str
+    volumeTitle: str
+    chapterTitle: str
+    sectionNumber: str
+    startLineNumber: int
+    content: str
+    cluster: int
+
+
+class DocListItem(BaseModel):
+    id: str
+    volumeTitle: str
+    chapterTitle: str
+    sectionNumber: str
+    cluster: int
+
+
+@router.get("/doc", response_model=list[DocListItem])
+def documents():
     """ドキュメントの一覧"""
-    return {
-        "documents": [
-            {
-                "id": item["id"],
-                "volumeTitle": item["volumeTitle"],
-                "chapterTitle": item["chapterTitle"],
-                "sectionNumber": item["sectionNumber"],
-                "cluster": item["cluster"],
-            }
-            for item in db.documents
-        ]
-    }
+    return [
+        {
+            "id": item["id"],
+            "volumeTitle": item["volumeTitle"],
+            "chapterTitle": item["chapterTitle"],
+            "sectionNumber": item["sectionNumber"],
+            "cluster": item["cluster"],
+        }
+        for item in db.documents
+    ]
 
 
-@router.get("/doc/{id}")
+@router.get("/doc/{id}", response_model=Doc)
 def document(id: str):
     """ドキュメントを1つ返す"""
     doc = next((item for item in db.documents if item["id"] == id), None)
     if doc is None:
         return JSONResponse(content={"notFound": id}, status_code=404)
-    return {"document": doc}
+    return doc
