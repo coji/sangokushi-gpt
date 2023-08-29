db49298c636f9518106171625475e5a104854bfc
coji <coji@techtalk.jp>
2023-08-29 11:11:55 +0900

    typed

--- api/src/sangokushi_api/routes/search.py
+++ api/src/sangokushi_api/routes/search.py
@@ -1,10 +1,21 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from ..services.database import db
+from .doc import Doc
 
 router = APIRouter()
 
 
-@router.get("/search")
+class ScoredDoc(BaseModel):
+    document: Doc
+    score: float
+
+
+class SearchResponse(BaseModel):
+    result: list[ScoredDoc]
+
+
+@router.get("/search", response_model=SearchResponse)
 def search(q: str, top_k: int = 10):
     """文章を受け取り、類似度の高い文章を返す"""
     result = db.query(q, top_k)
